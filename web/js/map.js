14b0859ada87843e6c2dc249a98ba5bc97b2a1d7
flyaway1217 <flyaway1217@gmail.com>
2017-11-14 21:00:24 -0700

    start writing for map

--- web/js/map.js
+++ web/js/map.js
@@ -1,5 +1,11 @@
 class Map {
     constructor(){
+        this.svg = d3.select('#map');
+    }
 
+    drawMap(){
+        let projection = d3.geoAlbersUsa();
+        let path = d3.geoPath()
+            .projection(projection);
     }
 }
