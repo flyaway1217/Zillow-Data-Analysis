ba7649d760fa0c4f8c5e9d323a6393253e055c3f
hanhanutah <h.han@utah.edu>
2017-11-23 14:45:35 -0700

    update script.js

--- web/js/script.js
+++ web/js/script.js
@@ -17,10 +17,43 @@ class Data{
 }
 */
 
+
+
+
+
 path = 'data/data.json';
 d3.json(path, function(data){
-    console.log(data);
-    let map = new Map();
-    map.drawMap();
-});
+    //console.log(data);
+    //console.log(data["ListPricePerSqft_1Bedroom"])
+    d3.select("body").select("#hometypebutton").on("click",function(){
+        let selected_type = []
 
+        for (let i =0;i < document.getElementsByName("Home_Type").length;i++){
+            if(document.getElementsByName("Home_Type")[i].checked){
+                selected_type.push(document.getElementsByName("Home_Type")[i].value)
+            }
+
+        }
+        //console.log(getSelectedValue("Home_Type"))
+
+        drawlinechart_houseprice(data,selected_type,[])
+
+    })
+    /*
+    d3.select("body").select("#economychoicebutton").on("click",function(){
+
+        let selected = []
+
+        for (let i =0;i < document.getElementsByName("choice").length;i++){
+            if(document.getElementsByName("choice")[i].checked){
+                selected.push(document.getElementsByName("choice")[i].value)
+            }
+
+        }
+        //console.log(selected)
+        drawlinechart("data/economy/ecodata.csv",selected)
+
+    })
+    */
+    }
+);
